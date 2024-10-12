75c6d491eb1dfaf9f4efe5b02cec562ff5a3668a
lellopastellone <l.sigalini001@studenti.unibs.it>
2024-10-12 18:17:36 +0200

    first commit

---
+++ src/it/unibs/pajc/GraphicsApp.java
@@ -0,0 +1,47 @@
+package it.unibs.pajc;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+
+public class GraphicsApp {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GraphicsApp window = new GraphicsApp();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GraphicsApp() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		PnlGraphics pnlGraphics = new PnlGraphics();
+		frame.getContentPane().add(pnlGraphics, BorderLayout.CENTER);
+	}
+
+}
---
+++ src/it/unibs/pajc/PnlGraphics.java
@@ -0,0 +1,48 @@
+package it.unibs.pajc;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class PnlGraphics extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * Create the panel.
+	 */
+	public PnlGraphics() {
+
+	}
+	
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		int w = getWidth();
+		int h = getHeight();
+		
+		int wSection = w/8;
+		int hSection = h/8;
+		
+		//g.drawLine(0, 0, w, h);
+		
+		g.setColor(Color.black);
+		g.fillRect(0, 0, w, h);
+		
+		for (int i = 1;i <= 7;i++) {
+			g.drawLine(0, hSection * i, w, hSection * i);
+			g.drawLine(wSection * i, 0, wSection * i, h);
+		}
+		
+		for (int i = 0; i <= 7; i++) {
+			for (int j = 0; i <= 7; j++) {
+				if((i + j) % 2 == 0) {
+					g.fillRect(wSection * j, hSection * i, wSection, hSection);
+				}
+			}
+		}
+		
+	}
+}
